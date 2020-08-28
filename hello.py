63bf013948bc97727b58e090d7dbc48d188314d2
xiidot1303 <65145330+xiidot1303@users.noreply.github.com>
2020-08-28 17:41:57 +0500

    =
    =

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
  def test_video_to_dict(self):
-        video = telegram.InlineQueryResultVideo.de_json(self.json_dict, self._bot).to_dict()
+        video = telegram.InlineQueryResultVideo.de_json(self.json_dict, self._bot).to_dict()====
 
         self.assertTrue(self.is_dict(video))
         self.assertDictEqual(self.json_dict, video)
